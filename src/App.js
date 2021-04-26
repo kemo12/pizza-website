e1dffb57c4b3c9bf945196e3532622f46d7471ef
kemo12 <dodkamel919@gmail.com>
2021-04-26 17:43:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router} from "react-router-dom";
+import { GlobalStyle } from "./globalStyles";
+function App() {
+  return (
+    <>
+    <Router>
+    <GlobalStyle/>
+    <Navbar/>
+    </Router>
+    </>
+
+  );
+}
+
+export default App;
